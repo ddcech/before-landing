de313ba432304ea934b3353e15db5aea05fae26b
David Cech <lilcech@gmail.com>
2019-11-22 13:47:19 +0100

    convert to typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,36 +2,37 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Portfolio'
+    title: 'Portfolio',
   },
   plugins: [
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'TESTS',
         short_name: 'starter',
         start_url: '/',
-        display: 'minimal-ui'
+        display: 'minimal-ui',
         // icon: 'static/assets/android-chrome-512x512.png', // This path is relative to the root of the site.
-      }
+      },
     },
     'gatsby-plugin-netlify-cms',
     'gatsby-transformer-json',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`
-      }
+        name: 'pages',
+        path: `${__dirname}/src/pages`,
+      },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    'gatsby-plugin-typescript',
   ],
 }
